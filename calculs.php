644875c5f65844559ee3466459a4af9d7692b507
jrgd <jrgd@mac.mini.studio>
2021-11-04 15:41:43 -0400

    init; dictee de chiffre (learning numbers in french); calculation tables

---
+++ calculs.php
@@ -0,0 +1,109 @@
+<?php
+
+
+// Operations Arithmétiques
+// un programme pour creer des exercices de tables d'operations arithmétique aléatoires
+
+// Regles du jeux
+//
+// à chaque colonne correspond 
+//  - à une operation arithmétique 
+//      + - / *
+//  - et à une valeur
+//      [0-9].*
+// chaque rangée possède au minimum une valeur affichée
+
+// how many columns → loop
+// foreach columns
+// 1. pick a random operator
+// 2. pick a value
+// how many rows
+// for each rows
+// 1. pick a starting value
+// 2. make the calculation
+// generate the exercice table
+// output only one value visible per row (random column index)
+// generate the auto-correction table
+// output everything
+
+$operators = ['-', '+', '*']; // '/', 
+
+$howmany_cols = 5;
+$howmany_rows = 20;
+$min_value = 1;
+$max_value = 20;
+
+$max_operator_index= count($operators)-1;
+
+// init
+$calculus = array();
+$results = array();
+for($row_i=1; $row_i<$howmany_rows+1; $row_i++) {
+  $calculus[0][$row_i] = rand($min_value, $max_value);
+}
+
+for ($col_i=1; $col_i<$howmany_cols+1; $col_i++ ){
+  $random_operator = rand(0,$max_operator_index);
+  $random_value = rand($min_value,$max_value);
+
+  $calculus_header[$col_i] = $operators[ $random_operator ] . " " . $random_value;
+
+  for($row_i=1; $row_i<$howmany_rows+1; $row_i++) {
+
+
+    // should refer to previous column, what for the first calculus? what for the remaining?
+    // we should have a first loop to define the start val outside of the calculation: stored in $calculus[0]
+    $calculus[$col_i][$row_i] = $calculus[$col_i-1][$row_i] . " " . $operators[ $random_operator ] . " " . $random_value;
+    $results[$col_i][$row_i] = eval("return ".$calculus[$col_i][$row_i].";");
+  }
+}
+
+system('clear');
+
+echo "Exercices\n";
+echo "\n";
+echo "\t";
+for ($col_i=1; $col_i<$howmany_cols+1; $col_i++ ){
+  echo $calculus_header[$col_i] . "\t";
+}
+echo "\n";
+echo "\t";
+for ($col_i=1; $col_i<$howmany_cols+1; $col_i++ ){
+  echo "------\t";
+}
+echo "\n";
+
+
+for($row_i=1; $row_i<$howmany_rows+1; $row_i++) {
+  $visible_col = rand(0, $howmany_cols);
+
+  for ($col_i=0; $col_i<$howmany_cols+1; $col_i++ ){
+    if ($col_i == $visible_col) {
+      echo eval('return (string) '.$calculus[$col_i][$row_i].';');
+    }
+    echo "\t";
+    
+  }
+  echo "\n";
+}
+
+echo "\n\n";
+echo "Corrections\n";
+echo "\n";
+echo "\t";
+for ($col_i=1; $col_i<$howmany_cols+1; $col_i++ ){
+  echo $calculus_header[$col_i] . "\t";
+}
+echo "\n";
+echo "\t";
+for ($col_i=1; $col_i<$howmany_cols+1; $col_i++ ){
+  echo "------\t";
+}
+echo "\n";
+for($row_i=1; $row_i<$howmany_rows+1; $row_i++) {
+  for ($col_i=0; $col_i<$howmany_cols+1; $col_i++ ){
+    echo eval('return '.$calculus[$col_i][$row_i].';');
+    echo "\t";
+  }
+  echo "\n";
+}
---
+++ rand_int_list.php
@@ -0,0 +1,99 @@
+<?php
+
+// DICTEES DE CHIFFRES
+// Learning numbers in french is tough from an english-native point of view
+// I wrote this to assist my daughter and generate randoim list of numbers for her to train
+// picks 10 4-digits random numbers
+// i would like the two last digits of those numbers to be above 59 and below 99
+// it can have any 1-9 for the first digit (hundreds)
+// the list should then be saved for correction at a later stage
+
+// How many numbers in this list?
+$quantity = 50;
+
+for($ii=0; $ii<$quantity; $ii++) {
+  $random_base_d1 = rand(0,9);
+  $random_base_d2 = rand(6,9);
+  $random_base_d3 = rand(1,9);
+
+  $random_number_list[$ii] = $random_base_d3 . $random_base_d2 . $random_base_d1;
+
+  $dict_unit = [
+    '0' => '',
+    '1' => 'un',
+    '2' => 'deux',
+    '3' => 'trois',
+    '4' => 'quatre',
+    '5' => 'cinq',
+    '6' => 'six',
+    '7' => 'sept',
+    '8' => 'huit',
+    '9' => 'neuf'
+  ];
+
+  $dict_unit_alt = [
+    '0' => 'dix',
+    '1' => 'onze',
+    '2' => 'douxe',
+    '3' => 'treize',
+    '4' => 'quatorze',
+    '5' => 'quinze',
+    '6' => 'seize',
+    '7' => 'dix-sept',
+    '8' => 'dix-huit',
+    '9' => 'dix-neuf'
+  ];
+
+  $dict_tenth = [
+    '0' => '',
+    '1' => 'dix',
+    '2' => 'vingt',
+    '3' => 'trente',
+    '4' => 'quarante',
+    '5' => 'cinquante',
+    '6' => 'soixante',
+    '7' => 'soixante',
+    '8' => 'quatre-vingt',
+    '9' => 'quatre-vingt'
+  ];
+
+  $dict_hundredth = [
+    '0' => '',
+    '1' => 'cent',
+    '2' => $dict_unit[2] . ' cent',
+    '3' => $dict_unit[3] . ' cent',
+    '4' => $dict_unit[4] . ' cent',
+    '5' => $dict_unit[5] . ' cent',
+    '6' => $dict_unit[6] . ' cent',
+    '7' => $dict_unit[7] . ' cent',
+    '8' => $dict_unit[8] . ' cent',
+    '9' => $dict_unit[9] . ' cent'
+  ];
+
+  if ( $random_base_d2 == 1 || $random_base_d2 == 7 || $random_base_d2 == 9 ) {
+    $dict_unit_corrected = $dict_unit_alt;
+  } else {
+    $dict_unit_corrected = $dict_unit;
+  }
+
+  $random_number_word_list[$ii] = $dict_hundredth[$random_base_d3] .' '. $dict_tenth[$random_base_d2] .' '. $dict_unit_corrected[$random_base_d1];
+
+}
+
+$exercice_output = "";
+$correction_output = "";
+for($ii=0; $ii<$quantity; $ii++) {
+  $exercice_output .= ($ii+1) .': ' . $random_number_word_list[$ii] . " → _ _ _ \n"; // '. $random_number_list[$ii] . '
+  $correction_output .= ($ii+1) .': ' . $random_number_word_list[$ii] . " → ". $random_number_list[$ii] . " \n"; // '. $random_number_list[$ii] . '
+}
+
+system('clear');
+
+echo "  \n";
+echo "  Exercices \n";
+echo "  \n";
+echo $exercice_output;
+echo "  \n";
+echo "  Corrections \n";
+echo "  \n";
+echo $correction_output;
